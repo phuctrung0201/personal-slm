48b28cd95a22fae1eecd86866e226e7a0ee85bcf
Trung Bui <mygalaxy2712@gmail.com>
2024-07-21 15:26:01 +0700

    init

---
+++ knowledge.py
@@ -0,0 +1,10 @@
+profile = """
+My name is Trung Bui, I was born in 1 Jan 2000 in Vietnam.
+I was a student at the University of Information Technology, VNU-HCM.
+I have graduated from the university in 2023.
+My first company is Skedulo, I worked as a backend engineer.
+I joined Skedulo in 2022 and worked there for 1 and a half years.
+In Skedulo, I was in CX team.
+I had a lost of colleges at Skedulo, they are very friendly and helpful.
+Although I am a backend engineer, I'm so close to the frontend team. 
+"""
---
+++ main.py
@@ -0,0 +1,36 @@
+import torch
+import knowledge
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+torch.random.manual_seed(0)
+
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3-mini-4k-instruct",
+    device_map="mps",
+    torch_dtype="auto",
+    trust_remote_code=True,
+)
+
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+
+messages = [
+    {"role": "system", "content": "Your are a python developer."},
+    {"role": "user", "content": "Help me generate a bubble algorithm"},
+]
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+)
+
+generation_args = {
+    "max_new_tokens": 600,
+    "return_full_text": False,
+    "temperature": 0,
+    "do_sample": False,
+}
+
+output = pipe(messages, **generation_args)
+print(output[0]['generated_text'])
